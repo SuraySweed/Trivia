97f607b5fd021f952c4e1a27bfa76f4a17428bbd
rabeaZezo <rabea0102.ghost@gmail.com>
2018-06-14 22:06:09 +0200

    client2.3

--- TriviaClient/Form1.Designer.cs
+++ TriviaClient/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.BackPic1 = new System.Windows.Forms.PictureBox();
             this.BackPic2 = new System.Windows.Forms.PictureBox();
             this.BackPic3 = new System.Windows.Forms.PictureBox();
@@ -38,6 +39,18 @@
             this.BackPic7 = new System.Windows.Forms.PictureBox();
             this.BackPic8 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SignUpButton = new System.Windows.Forms.Button();
+            this.CreateRoomButton = new System.Windows.Forms.Button();
+            this.JoinRoomButton = new System.Windows.Forms.Button();
+            this.MyStatusButton = new System.Windows.Forms.Button();
+            this.BestScoresButton = new System.Windows.Forms.Button();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.UserName = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.SignInButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.BackPic1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BackPic2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BackPic3)).BeginInit();
@@ -50,6 +63,7 @@
             // 
             // BackPic1
             // 
+            this.BackPic1.BackColor = System.Drawing.Color.Transparent;
             this.BackPic1.BackgroundImage = global::NewTriviaClient.Properties.Resources.MyQM;
             this.BackPic1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BackPic1.Location = new System.Drawing.Point(-11, 79);
@@ -60,6 +74,7 @@
             // 
             // BackPic2
             // 
+            this.BackPic2.BackColor = System.Drawing.Color.Transparent;
             this.BackPic2.BackgroundImage = global::NewTriviaClient.Properties.Resources.MyQM;
             this.BackPic2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BackPic2.Location = new System.Drawing.Point(247, -49);
@@ -70,16 +85,18 @@
             // 
             // BackPic3
             // 
+            this.BackPic3.BackColor = System.Drawing.Color.Transparent;
             this.BackPic3.BackgroundImage = global::NewTriviaClient.Properties.Resources.MyQM;
             this.BackPic3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.BackPic3.Location = new System.Drawing.Point(311, 552);
+            this.BackPic3.Location = new System.Drawing.Point(406, 398);
             this.BackPic3.Name = "BackPic3";
-            this.BackPic3.Size = new System.Drawing.Size(137, 138);
+            this.BackPic3.Size = new System.Drawing.Size(114, 123);
             this.BackPic3.TabIndex = 2;
             this.BackPic3.TabStop = false;
             // 
             // BackPic4
             // 
+            this.BackPic4.BackColor = System.Drawing.Color.Transparent;
             this.BackPic4.BackgroundImage = global::NewTriviaClient.Properties.Resources.MyQM;
             this.BackPic4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BackPic4.Location = new System.Drawing.Point(621, -71);
@@ -91,6 +108,7 @@
             // 
             // BackPic5
             // 
+            this.BackPic5.BackColor = System.Drawing.Color.Transparent;
             this.BackPic5.BackgroundImage = global::NewTriviaClient.Properties.Resources.MyQM;
             this.BackPic5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BackPic5.Location = new System.Drawing.Point(76, 385);
@@ -101,6 +119,7 @@
             // 
             // BackPic6
             // 
+            this.BackPic6.BackColor = System.Drawing.Color.Transparent;
             this.BackPic6.BackgroundImage = global::NewTriviaClient.Properties.Resources.MyQM;
             this.BackPic6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BackPic6.Location = new System.Drawing.Point(556, 151);
@@ -111,6 +130,7 @@
             // 
             // BackPic7
             // 
+            this.BackPic7.BackColor = System.Drawing.Color.Transparent;
             this.BackPic7.BackgroundImage = global::NewTriviaClient.Properties.Resources.MyQM;
             this.BackPic7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.BackPic7.Location = new System.Drawing.Point(595, 449);
@@ -121,9 +141,10 @@
             // 
             // BackPic8
             // 
+            this.BackPic8.BackColor = System.Drawing.Color.Transparent;
             this.BackPic8.BackgroundImage = global::NewTriviaClient.Properties.Resources.MyQM;
             this.BackPic8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.BackPic8.Location = new System.Drawing.Point(340, 220);
+            this.BackPic8.Location = new System.Drawing.Point(307, 175);
             this.BackPic8.Name = "BackPic8";
             this.BackPic8.Size = new System.Drawing.Size(137, 128);
             this.BackPic8.TabIndex = 7;
@@ -135,12 +156,166 @@
             this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // SignUpButton
+            // 
+            this.SignUpButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.SignUpButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.SignUpButton.Font = new System.Drawing.Font("Lucida Blackletter", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SignUpButton.ForeColor = System.Drawing.Color.White;
+            this.SignUpButton.Location = new System.Drawing.Point(288, 209);
+            this.SignUpButton.Name = "SignUpButton";
+            this.SignUpButton.Size = new System.Drawing.Size(282, 56);
+            this.SignUpButton.TabIndex = 8;
+            this.SignUpButton.Text = "Sign Up";
+            this.SignUpButton.UseVisualStyleBackColor = false;
+            // 
+            // CreateRoomButton
+            // 
+            this.CreateRoomButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.CreateRoomButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.CreateRoomButton.Font = new System.Drawing.Font("Lucida Blackletter", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateRoomButton.ForeColor = System.Drawing.Color.White;
+            this.CreateRoomButton.Location = new System.Drawing.Point(102, 320);
+            this.CreateRoomButton.Name = "CreateRoomButton";
+            this.CreateRoomButton.Size = new System.Drawing.Size(282, 59);
+            this.CreateRoomButton.TabIndex = 9;
+            this.CreateRoomButton.Text = "Create Room";
+            this.CreateRoomButton.UseVisualStyleBackColor = false;
+            // 
+            // JoinRoomButton
+            // 
+            this.JoinRoomButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.JoinRoomButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.JoinRoomButton.Font = new System.Drawing.Font("Lucida Blackletter", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.JoinRoomButton.ForeColor = System.Drawing.Color.White;
+            this.JoinRoomButton.Location = new System.Drawing.Point(476, 320);
+            this.JoinRoomButton.Name = "JoinRoomButton";
+            this.JoinRoomButton.Size = new System.Drawing.Size(282, 59);
+            this.JoinRoomButton.TabIndex = 10;
+            this.JoinRoomButton.Text = "Join Room";
+            this.JoinRoomButton.UseVisualStyleBackColor = false;
+            // 
+            // MyStatusButton
+            // 
+            this.MyStatusButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.MyStatusButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.MyStatusButton.Font = new System.Drawing.Font("Lucida Blackletter", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MyStatusButton.ForeColor = System.Drawing.Color.White;
+            this.MyStatusButton.Location = new System.Drawing.Point(102, 423);
+            this.MyStatusButton.Name = "MyStatusButton";
+            this.MyStatusButton.Size = new System.Drawing.Size(282, 51);
+            this.MyStatusButton.TabIndex = 11;
+            this.MyStatusButton.Text = "My Status";
+            this.MyStatusButton.UseVisualStyleBackColor = false;
+            // 
+            // BestScoresButton
+            // 
+            this.BestScoresButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.BestScoresButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BestScoresButton.Font = new System.Drawing.Font("Lucida Blackletter", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BestScoresButton.ForeColor = System.Drawing.Color.White;
+            this.BestScoresButton.Location = new System.Drawing.Point(476, 422);
+            this.BestScoresButton.Name = "BestScoresButton";
+            this.BestScoresButton.Size = new System.Drawing.Size(282, 52);
+            this.BestScoresButton.TabIndex = 12;
+            this.BestScoresButton.Text = "Best Scores";
+            this.BestScoresButton.UseVisualStyleBackColor = false;
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.BackColor = System.Drawing.Color.Peru;
+            this.QuitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.QuitButton.Font = new System.Drawing.Font("Lucida Blackletter", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuitButton.ForeColor = System.Drawing.Color.White;
+            this.QuitButton.Location = new System.Drawing.Point(371, 527);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(119, 42);
+            this.QuitButton.TabIndex = 13;
+            this.QuitButton.Text = "Quit";
+            this.QuitButton.UseVisualStyleBackColor = false;
+            this.QuitButton.Click += new System.EventHandler(this.QuitButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(185, 134);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(365, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(185, 160);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(365, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // UserName
+            // 
+            this.UserName.AutoSize = true;
+            this.UserName.BackColor = System.Drawing.Color.Transparent;
+            this.UserName.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserName.ForeColor = System.Drawing.Color.Black;
+            this.UserName.Location = new System.Drawing.Point(103, 134);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(76, 17);
+            this.UserName.TabIndex = 16;
+            this.UserName.Text = "USERNAME";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.BackColor = System.Drawing.Color.Transparent;
+            this.Password.Font = new System.Drawing.Font("Showcard Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.ForeColor = System.Drawing.Color.Black;
+            this.Password.Location = new System.Drawing.Point(103, 160);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(82, 17);
+            this.Password.TabIndex = 17;
+            this.Password.Text = "PASSWORD";
+            // 
+            // SignInButton
+            // 
+            this.SignInButton.BackColor = System.Drawing.Color.SteelBlue;
+            this.SignInButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.SignInButton.Font = new System.Drawing.Font("Lucida Blackletter", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SignInButton.ForeColor = System.Drawing.Color.White;
+            this.SignInButton.Location = new System.Drawing.Point(556, 134);
+            this.SignInButton.Name = "SignInButton";
+            this.SignInButton.Size = new System.Drawing.Size(152, 53);
+            this.SignInButton.TabIndex = 18;
+            this.SignInButton.Text = "Sign In";
+            this.SignInButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Miriam Fixed", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Peru;
+            this.label1.Location = new System.Drawing.Point(90, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(678, 95);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Trivia Game";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(808, 606);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SignInButton);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.UserName);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.QuitButton);
+            this.Controls.Add(this.BestScoresButton);
+            this.Controls.Add(this.MyStatusButton);
+            this.Controls.Add(this.JoinRoomButton);
+            this.Controls.Add(this.CreateRoomButton);
+            this.Controls.Add(this.SignUpButton);
             this.Controls.Add(this.BackPic8);
             this.Controls.Add(this.BackPic7);
             this.Controls.Add(this.BackPic6);
@@ -149,8 +324,9 @@
             this.Controls.Add(this.BackPic3);
             this.Controls.Add(this.BackPic2);
             this.Controls.Add(this.BackPic1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "TriviaGame";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.BackPic1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.BackPic2)).EndInit();
@@ -161,6 +337,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.BackPic7)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.BackPic8)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -175,6 +352,18 @@
         private System.Windows.Forms.PictureBox BackPic7;
         private System.Windows.Forms.PictureBox BackPic8;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button SignUpButton;
+        private System.Windows.Forms.Button CreateRoomButton;
+        private System.Windows.Forms.Button JoinRoomButton;
+        private System.Windows.Forms.Button MyStatusButton;
+        private System.Windows.Forms.Button BestScoresButton;
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label UserName;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Button SignInButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TriviaClient/Form1.cs
+++ TriviaClient/Form1.cs
@@ -78,5 +78,10 @@ namespace NewTriviaClient
         {
 
         }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
