78d991b24d8b9b617b8a6006c2f786293dd1e234
rabeaZezo <rabea0102.ghost@gmail.com>
2018-06-14 16:35:35 +0200

    client

---
+++ TriviaClient/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TriviaClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BackPic4 = new System.Windows.Forms.PictureBox();
+            this.BackPic3 = new System.Windows.Forms.PictureBox();
+            this.BackPic2 = new System.Windows.Forms.PictureBox();
+            this.BackPic1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.BackPic5 = new System.Windows.Forms.PictureBox();
+            this.BackPic6 = new System.Windows.Forms.PictureBox();
+            this.BackPic8 = new System.Windows.Forms.PictureBox();
+            this.BackPic7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BackPic4
+            // 
+            this.BackPic4.BackgroundImage = global::TriviaClient.Properties.Resources.MyQM;
+            this.BackPic4.Location = new System.Drawing.Point(27, 229);
+            this.BackPic4.Name = "BackPic4";
+            this.BackPic4.Size = new System.Drawing.Size(140, 170);
+            this.BackPic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BackPic4.TabIndex = 1;
+            this.BackPic4.TabStop = false;
+            this.BackPic4.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // BackPic3
+            // 
+            this.BackPic3.BackgroundImage = global::TriviaClient.Properties.Resources.MyQM;
+            this.BackPic3.Location = new System.Drawing.Point(212, 77);
+            this.BackPic3.Name = "BackPic3";
+            this.BackPic3.Size = new System.Drawing.Size(140, 170);
+            this.BackPic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BackPic3.TabIndex = 2;
+            this.BackPic3.TabStop = false;
+            // 
+            // BackPic2
+            // 
+            this.BackPic2.BackgroundImage = global::TriviaClient.Properties.Resources.MyQM;
+            this.BackPic2.Location = new System.Drawing.Point(419, 229);
+            this.BackPic2.Name = "BackPic2";
+            this.BackPic2.Size = new System.Drawing.Size(140, 170);
+            this.BackPic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BackPic2.TabIndex = 3;
+            this.BackPic2.TabStop = false;
+            // 
+            // BackPic1
+            // 
+            this.BackPic1.BackgroundImage = global::TriviaClient.Properties.Resources.MyQM;
+            this.BackPic1.Location = new System.Drawing.Point(600, 77);
+            this.BackPic1.Name = "BackPic1";
+            this.BackPic1.Size = new System.Drawing.Size(140, 170);
+            this.BackPic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BackPic1.TabIndex = 4;
+            this.BackPic1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // BackPic5
+            // 
+            this.BackPic5.BackgroundImage = global::TriviaClient.Properties.Resources.MyQM;
+            this.BackPic5.Location = new System.Drawing.Point(634, 353);
+            this.BackPic5.Name = "BackPic5";
+            this.BackPic5.Size = new System.Drawing.Size(140, 170);
+            this.BackPic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BackPic5.TabIndex = 5;
+            this.BackPic5.TabStop = false;
+            // 
+            // BackPic6
+            // 
+            this.BackPic6.BackgroundImage = global::TriviaClient.Properties.Resources.MyQM;
+            this.BackPic6.Location = new System.Drawing.Point(229, 378);
+            this.BackPic6.Name = "BackPic6";
+            this.BackPic6.Size = new System.Drawing.Size(140, 170);
+            this.BackPic6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BackPic6.TabIndex = 6;
+            this.BackPic6.TabStop = false;
+            // 
+            // BackPic8
+            // 
+            this.BackPic8.BackgroundImage = global::TriviaClient.Properties.Resources.MyQM;
+            this.BackPic8.Location = new System.Drawing.Point(82, -72);
+            this.BackPic8.Name = "BackPic8";
+            this.BackPic8.Size = new System.Drawing.Size(140, 170);
+            this.BackPic8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BackPic8.TabIndex = 7;
+            this.BackPic8.TabStop = false;
+            // 
+            // BackPic7
+            // 
+            this.BackPic7.BackgroundImage = global::TriviaClient.Properties.Resources.MyQM;
+            this.BackPic7.Location = new System.Drawing.Point(482, -61);
+            this.BackPic7.Name = "BackPic7";
+            this.BackPic7.Size = new System.Drawing.Size(140, 170);
+            this.BackPic7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BackPic7.TabIndex = 8;
+            this.BackPic7.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::TriviaClient.Properties.Resources.SignUp2;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(320, 117);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(796, 491);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BackPic7);
+            this.Controls.Add(this.BackPic8);
+            this.Controls.Add(this.BackPic6);
+            this.Controls.Add(this.BackPic5);
+            this.Controls.Add(this.BackPic1);
+            this.Controls.Add(this.BackPic2);
+            this.Controls.Add(this.BackPic3);
+            this.Controls.Add(this.BackPic4);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Trivia";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox BackPic4;
+        private System.Windows.Forms.PictureBox BackPic3;
+        private System.Windows.Forms.PictureBox BackPic2;
+        private System.Windows.Forms.PictureBox BackPic1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox BackPic5;
+        private System.Windows.Forms.PictureBox BackPic6;
+        private System.Windows.Forms.PictureBox BackPic8;
+        private System.Windows.Forms.PictureBox BackPic7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TriviaClient/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriviaClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int NTop = 4;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            BackPic1.Top--;
+            if(BackPic1.Bottom <= 0)
+            {
+                BackPic1.Top = this.ClientRectangle.Bottom;
+            }
+
+            BackPic2.Top--;
+            if (BackPic2.Bottom <= 0)
+            {
+                BackPic2.Top = this.ClientRectangle.Bottom;
+            }
+
+            BackPic3.Top--;
+            if (BackPic3.Bottom <= 0)
+            {
+                BackPic3.Top = this.ClientRectangle.Bottom;
+            }
+
+            BackPic4.Top--;
+            if (BackPic4.Bottom <= 0)
+            {
+                BackPic4.Top = this.ClientRectangle.Bottom;
+            }
+
+            BackPic5.Top--;
+            if (BackPic5.Bottom <= 0)
+            {
+                BackPic5.Top = this.ClientRectangle.Bottom;
+            }
+
+
+            BackPic6.Top--;
+            if (BackPic6.Bottom <= 0)
+            {
+                BackPic6.Top = this.ClientRectangle.Bottom;
+            }
+
+            BackPic7.Top--;
+            if (BackPic7.Bottom <= 0)
+            {
+                BackPic7.Top = this.ClientRectangle.Bottom;
+            }
+
+            BackPic8.Top--;
+            if (BackPic8.Bottom <= 0)
+            {
+                BackPic8.Top = this.ClientRectangle.Bottom;
+            }
+        }
+    }
+}
