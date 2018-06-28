ab84cb6b07115acae102d4952b37c36022b0bdaf
rabeaZezo <rabea0102.ghost@gmail.com>
2018-06-28 12:52:01 +0200

    Forms!! Forms everyWhere!!!!

--- TriviaClient/myStatusForm.Designer.cs
+++ TriviaClient/myStatusForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(myStatusForm));
             this.BackButton = new System.Windows.Forms.Button();
             this.NameOfUser = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -91,6 +92,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.NameOfUser);
             this.Controls.Add(this.BackButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "myStatusForm";
             this.Text = "myStatusForm";
             this.ResumeLayout(false);
